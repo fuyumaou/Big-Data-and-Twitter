091e06135c6c4dd6c2c15c9db72279584553f5a7
Paul Herman <paulstefanherman@gmail.com>
2015-03-02 22:30:41 +0000

    downgrade socketio

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'va_%r-jZ%Yl=3t9Q8ml[.Wu0!mT$Gy[gsgr/:>M8rm-]0fq`^<TK2L*x"M"\dQW'
+app.config['SECRET_KEY'] = 'va_%r-jZ%Yl=3t9Q8ml[.Wu0!mT$Gy[gsgr/:>M8rm-]0fq`^<TK2L*x\dQW'
 socketio = SocketIO(app)
 
 @app.route('/')
 def view_index():
-    return render_template("feed.html")
+    return render_template('feed.html')
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, host = '0.0.0.0')
