3d0c49cb2ae94f90369c1b54717344619e71e45b
Paul Herman <paulstefanherman@gmail.com>
2015-03-02 21:02:50 +0000

    dependencies

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
+#!env/bin/python
 from flask import Flask, render_template, request, redirect, url_for
+from flask_socketio import SocketIO, emit
 
-ap == Flask(__name__)
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'va_%r-jZ%Yl=3t9Q8ml[.Wu0!mT$Gy[gsgr/:>M8rm-]0fq`^<TK2L*x"M"\dQW'
+socketio = SocketIO(app)
 
 @app.route('/')
 def view_index():
     return 'Hello world!'
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    socketio.run(app)
