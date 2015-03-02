b692bec102923788ff401259b845346d0083ee77
Paul Herman <paulstefanherman@gmail.com>
2015-03-02 22:45:23 +0000

    connect socketio test

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #!env/bin/python
 from flask import Flask, render_template, request, redirect, url_for
 from flask_socketio import SocketIO, emit
+import json
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'va_%r-jZ%Yl=3t9Q8ml[.Wu0!mT$Gy[gsgr/:>M8rm-]0fq`^<TK2L*x\dQW'
@@ -9,6 +10,10 @@ socketio = SocketIO(app)
 @app.route('/')
 def view_index():
     return render_template('feed.html')
+
+@socketio.on('search_hashtags')
+def ws_search_hashtags(data):
+    socketio.emit('tweets_update', data)
 
 if __name__ == '__main__':
     socketio.run(app, host = '0.0.0.0')
