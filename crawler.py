f8552db1cd0d325ab01b44fb2e8395ca4028d679
Paul Herman <paulstefanherman@gmail.com>
2015-04-01 18:44:39 +0100

    use twitter language detection mechanism

--- crawler.py
+++ crawler.py
@@ -1,15 +1,10 @@
 import re
-import langid
 from TwitterAPI import TwitterAPI
 
 twitter_access_token = '178658388-CDwtvkSOOb3ikZXaVeDBlxzHwj0wEyQ5ntTPhs5n'
 twitter_access_token_secret = 'zJzQK6F00hwsG32STbITqvavbhYt5rtV6vZH69QbcKf8I'
 twitter_consumer_key = 'bWMmJpHklikmU3fbKemgmr40H'
 twitter_consumer_secret = 'MsAYHkqUuGi1bBWiTyiJiDdVCQ6DvYMt8ROsjJ1GFIFQCFP0Dp'
-
-def tweet_text_language(tweet_text):
-	(language, confidence) = langid.classify(tweet_text)
-	return language
 
 def tweet_text_process(tweet_text):
 	tweet_text = re.sub(r'[^\x00-\x7F]+',' ', tweet_text)
@@ -20,15 +15,17 @@ def tweet_text_process(tweet_text):
 	tweet_words = filter(lambda w: w != '', tweet_words) # filter empty words
 	return ' '.join(tweet_words) # join words by a space
 
+def tweet_process(tweet):
+	tweet_text = tweet_text_process(tweet['text'])
+	tweet_language = None
+	if tweet['lang'] != 'und':
+		tweet_language = tweet['lang']
+	print(tweet_text, tweet_language)
+
 
 if __name__ == '__main__':
 	twitter_api = TwitterAPI(twitter_consumer_key, twitter_consumer_secret, twitter_access_token, twitter_access_token_secret)
 	twitter_stream = twitter_api.request('statuses/filter', {'locations': '-14.02,49.67,2.09,61.06'}) # get tweets stream for UK
 	for tweet in twitter_stream:
 		if 'text' in tweet:
-			tweet_text = tweet_text_process(tweet['text'])
-			tweet_language = tweet_text_language(tweet_text)
-			if tweet_language != 'en':
-				print tweet_text
-				print tweet_language
-
+			tweet_process(tweet)
