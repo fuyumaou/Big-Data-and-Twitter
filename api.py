112d8bbebd3d6d6e5b293ab4a2d670154e1da3aa
Maou-San <dan-andrei.gheorghe@some.ox.ac.uk>
2015-04-02 16:21:31 +0300

    Added three more GET requests to API (All tweets languages and their locations in rectangular and circular area, language stats for tweets in a circular area).

--- api.py
+++ api.py
@@ -19,7 +19,7 @@ languages = ['en']
 # !! --- need to be changed to work with an actual MongoDB database
 # Input: x0, y0, x1, y1 --- 4 coordinates
 # Output: A List of Records of type {language, number} where number is the number
-#		of tweets in the respective language
+#		of tweets in the respective language in the defined rectangular area
 def helper_languages_get(x0,y0,x1,y1):
 	results=[]
 	for lang in languages:
@@ -27,16 +27,75 @@ def helper_languages_get(x0,y0,x1,y1):
 		if len>0:
 			results.append({'language':lang,'number':number_tweets_in_lang})
 	return results
+	
+# helper_languagescircle_get
+# !! --- need to be changed to work with an actual MongoDB database
+# Input: x,y --- 2 coordinates, r --- radius of the area
+# Output: A List of Records of type {language, number} where number is the number
+#		of tweets in the respective language in the defined circular area
+def helper_languagescircle_get(x,y,r):
+	results=[]
+	for lang in languages:
+		number_tweets_in_lang=len(filter(lambda p: (p['language']==lang and (p['x']-x)**2+(p['y']-y)**2<=r**2), db))
+		if len>0:
+			results.append({'language':lang,'number':number_tweets_in_lang})
+	return results
+
+# helper_languageslocations_get
+# !! --- need to be changed to work with an actual MongoDB database
+# !! --- naming needs to be changed here and in the actual html doc to be more relevant
+# Input: x0, y0, x1, y1 --- 4 coordinates
+# Output: A List of Records of type {type, properties:{language}, geometry: {type,coordinates}}
+#			that correspond to tweets, their languages and their locations that can be found in the rectangular area x0,y0,x1,y1
+def helper_languageslocations_get(x0,y0,x1,y1):
+	results=[]
+	for tweet in (filter(lambda p: (p['x']>=x0 and p['x']<=x1 and p['y']>=y0 and p['y']<=y1),db)):
+		results.append({'type': 'Feature','properties':{
+		  'language':tweet['language']}, 'geometry': {'type': 'Point',
+		    'coordinates': [tweet['x'], tweet['y']]}})
+	return results
+	
+# helper_languageslocationscircle_get
+# !! --- need to be changed to work with an actual MongoDB database
+# !! --- naming needs to be changed here and in the actual html doc to be more relevant
+# Input: x,y --- 2 coordinates, r --- radius of the area
+# Output: A List of Records of type {type, properties:{language}, geometry: {type,coordinates}}
+#			that correspond to tweets, their languages and their locations that can be found in the circular area defined by x,y,r
+def helper_languageslocationscircle_get(x,y,r):
+	results=[]
+	for tweet in (filter(lambda p: ((p['x']-x)**2+(p['y']-y)**2<=r**2), db)):
+		results.append({'type': 'Feature','properties':{
+		  'language':tweet['language']}, 'geometry': {'type': 'Point',
+		    'coordinates': [tweet['x'], tweet['y']]}})
+	return results
 
 #----------------------------------------------------------------------------
 
 #----------------------------------------------------------------------------
 
-# GET request for the languages in the area x0, y0, x1, y1
+# GET request for the languages in the rectangular area x0, y0, x1, y1
 @app.route('/languages/<int:x0>/<int:y0>/<int:x1>/<int:y1>', methods = ['GET'])
 def api_languages_get(x0,y0,x1,y1):
 	results = helper_languages_get(x0,y0,x1,y1)
-	return make_response(jsonify({'stats':results}), 200)
+	return make_response(jsonify({'type':'LanguagesCounted','features':results}), 200)
+
+# GET request for the languages in the circular area with centre (x, y) and radius r
+@app.route('/languagescircle/<int:x>/<int:y>/<int:r>', methods = ['GET'])
+def api_languagescircle_get(x,y,r):
+	results = helper_languagescircle_get(x,y,r)
+	return make_response(jsonify({'type':'LanguagesCounted','features':results}), 200)
+	
+# GET request for the locations of all tweets and their language in the rectangular area x0, y0, x1, y1
+@app.route('/languageslocations/<int:x0>/<int:y0>/<int:x1>/<int:y1>', methods = ['GET'])
+def api_languageslocations_get(x0,y0,x1,y1):
+	results = helper_languageslocations_get(x0,y0,x1,y1)
+	return make_response(jsonify({'type':'LanguagesLocations','features':results}),200)
+	
+# GET request for the locations of all tweets and their language in circular area with centre (x,y) and radius r**2
+@app.route('/languageslocationscircle/<int:x>/<int:y>/<int:r>', methods =['GET'])
+def api_languaceslocationscircle_get(x,y,r):
+	results = helper_languageslocationscircle_get(x,y,r)
+	return make_response(jsonify({'type':'LanguagesLocations','features':results}),200)
 
 #-----------------------------------------------------------------------------
 
