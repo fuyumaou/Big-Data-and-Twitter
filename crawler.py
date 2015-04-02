7cb51cc2a1132516823d2f2ef5cd6619b70e75e3
Paul Herman <paulstefanherman@gmail.com>
2015-04-02 21:29:57 +0100

    mongo language count

--- crawler.py
+++ crawler.py
@@ -1,6 +1,7 @@
 import re
 import os
 from TwitterAPI import TwitterAPI
+from pymongo import MongoClient
 
 twitter_access_token = '178658388-CDwtvkSOOb3ikZXaVeDBlxzHwj0wEyQ5ntTPhs5n'
 twitter_access_token_secret = 'zJzQK6F00hwsG32STbITqvavbhYt5rtV6vZH69QbcKf8I'
@@ -16,15 +17,23 @@ def tweet_text_words(tweet_text):
 	tweet_words = filter(lambda w: w != '', tweet_words) # filter empty words
 	return tweet_words
 
-def tweet_process(tweet, stopwords):
+def tweet_process(tweet, stopwords, mongo_db):
 	tweet_text_dirty = tweet['text']
 	tweet_words = tweet_text_words(tweet_text_dirty)
 	tweet_text = ' '.join(tweet_words)
-	tweet_language = None
 	tweet_non_stopwords = filter(lambda w: w not in stopwords, tweet_words)
 
-	if tweet['lang'] != 'und':
-		tweet_language = tweet['lang']
+	mongo_db_languages = mongo_db['languages']
+
+	tweet_language = tweet['lang']
+	if tweet_language != 'und':
+		mongo_db_languages.update({
+			'language': tweet_language
+		}, {
+			'$push': {
+				'tweets': tweet_text
+			}
+		}, True)
 
 	print(tweet_text, tweet_language, tweet_non_stopwords)
 
@@ -35,11 +44,12 @@ def read_stopwords():
 
 
 if __name__ == '__main__':
-	mongodb_url = os.getenv('MONGOLAB_URI')
-	print mongodb_url
+	mongo_url = os.getenv('MONGOLAB_URI')
+	mongo_client = MongoClient(mongo_url)
+	mongo_db = mongo_client.get_default_database()
 	stopwords = read_stopwords()
 	twitter_api = TwitterAPI(twitter_consumer_key, twitter_consumer_secret, twitter_access_token, twitter_access_token_secret)
 	twitter_stream = twitter_api.request('statuses/filter', {'locations': '-14.02,49.67,2.09,61.06'}) # get tweets stream for UK
 	for tweet in twitter_stream:
 		if 'text' in tweet:
-			tweet_process(tweet, stopwords)
+			tweet_process(tweet, stopwords, mongo_db)
