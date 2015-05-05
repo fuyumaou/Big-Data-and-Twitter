65312cd2e1f8c6b2b43a954fd5dd5e035e8449a3
Paul Herman <paulstefanherman@gmail.com>
2015-05-05 11:20:41 +0100

    fix sentiment bar; fix word cloud

--- static/js/map.js
+++ static/js/map.js
@@ -1,74 +1,75 @@
-var input = document.getElementById( "pac-input" );
+var input = document.getElementById("pac-input");
 
 var initializeMap = function() {
 	//set up the map
 	//----------------
-	var styles = [
-		{
-			"featureType": "road",
-			"elementType": "geometry",
-			"stylers": [
-				{ "hue": "#cc00ff" },
-				{ "saturation": -74 },
-				{ "lightness": 23 }
-			]
+	var styles = [{
+		"featureType": "road",
+		"elementType": "geometry",
+		"stylers": [{
+			"hue": "#cc00ff"
 		}, {
-			"featureType": "landscape",
-			"stylers": [
-				{ "saturation": -100 },
-				{ "lightness": 50 }
-			]
+			"saturation": -74
 		}, {
-			"featureType": "poi",
-			"elementType": "geometry",
-			"stylers": [
-				{ "hue": "#55ff00" },
-				{ "saturation": 67 }
-			]
+			"lightness": 23
+		}]
+	}, {
+		"featureType": "landscape",
+		"stylers": [{
+			"saturation": -100
 		}, {
-			"featureType": "transit",
-			"stylers": [
-				{ "visibility": "off" }
-			]
+			"lightness": 50
+		}]
+	}, {
+		"featureType": "poi",
+		"elementType": "geometry",
+		"stylers": [{
+			"hue": "#55ff00"
 		}, {
-			"featureType": "water",
-			"stylers": [
-				{ "hue": "#0077ff" }
-			]
-		}, {
-			"featureType": "road",
-			"elementType": "labels",
-			"stylers": [
-				{ "visibility": "off" }
-			]
-		}, {
-			"featureType": "road.highway",
-			"stylers": [
-				{ "lightness": 21 }
-			]
-		}
-	];
+			"saturation": 67
+		}]
+	}, {
+		"featureType": "transit",
+		"stylers": [{
+			"visibility": "off"
+		}]
+	}, {
+		"featureType": "water",
+		"stylers": [{
+			"hue": "#0077ff"
+		}]
+	}, {
+		"featureType": "road",
+		"elementType": "labels",
+		"stylers": [{
+			"visibility": "off"
+		}]
+	}, {
+		"featureType": "road.highway",
+		"stylers": [{
+			"lightness": 21
+		}]
+	}];
 
 	var mapOptions = {
 		center: {
 			lat: 46.78003,
 			lng: 7.96637
-		}, zoom: 8,
+		},
+		zoom: 8,
 		minZoom: 5,
 		disableDefaultUI: true,
 		styles: styles
 	};
 
-	 map = new google.maps.Map( document.getElementById( "map-canvas" ), mapOptions );
+	map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
-	
-	
-	
-	
-	
+
+
+
 	//Stuff used in the places tab
 	//----------------------------
-	
+
 	//help from http://stackoverflow.com/a/24234818/1779797
 	fx = google.maps.InfoWindow.prototype.setPosition
 	google.maps.InfoWindow.prototype.setPosition = function() {
@@ -82,31 +83,32 @@ var initializeMap = function() {
 			var infoWindow = this;
 			var name = infoWindow.getContent().firstChild.firstChild.nodeValue;
 			var pos = infoWindow.position;
+			var sentimentBar = $("#sentiment-canvas").sentimentBar();
 			console.log(name);
 			console.log(pos.lat() + ", " + pos.lng())
 			$.get("/place/" + name + "/" + pos.lat() + "/" + pos.lng(), function(response) {
 				console.log(response);
-				if(response.account_id){
-					$("#places-account").html('<a class="twitter-timeline" href="https://twitter.com/tcb1024" data-widget-id="594975506904256512"   data-user-id="'+response.account_id+'">Tweets by +'+response.account_name+'+</a>')
+				if (response.account_id) {
+					$("#places-account").html('<a class="twitter-timeline" href="https://twitter.com/tcb1024" data-widget-id="594975506904256512"   data-user-id="' + response.account_id + '">Tweets by +' + response.account_name + '+</a>')
 					twttr.widgets.load()
-				}
-				else {
+				} else {
 					$("#places-account").html('');
 				}
 
-				$("#places-content").html('Average Tweet Sentiment: ' + response.average_sentiment + '<br />+' + response.positive_sentiments + '-' + response.negative_sentiments);
+				$("#places-content").html('Average Tweet Sentiment: ' + Math.round(response.average_sentiment * 100) / 100 + ' / 10');
+				sentimentBar.setValue(response.average_sentiment);
 				//TODO: make this red to green rather than a number
 				// http://wbotelhos.com/raty
 				//TODO: display thumbsup vs thumbsdown (imgs already on server)
 			})
 		}
 	}
-	
-	
-	
+
+
+
 	//Stuff used for the languages tab
 	//--------------------------------
-	
+
 	var flags = {
 		"en": "/static/img/flag-en.png",
 		"fr": "/static/img/flag-fr.png",
@@ -114,25 +116,29 @@ var initializeMap = function() {
 		"de": "/static/img/flag-de.png",
 		"it": "/static/img/flag-it.png"
 	};
-	map.data.setStyle( function( feature ) {
-		var lang = feature.getProperty( "language" );
+	map.data.setStyle(function(feature) {
+		var lang = feature.getProperty("language");
 		var icon = "/static/img/dot.png";
-		if ( lang in flags ) { icon = flags[lang]; }
-		return { "icon": icon };
-	} );
+		if (lang in flags) {
+			icon = flags[lang];
+		}
+		return {
+			"icon": icon
+		};
+	});
 	//get all the tweets
-	 tweets = []
-	$.get('/allTweetLangs',function(response){
-		tweets=response.tweets
-		gj=[]
-		for (tweetn in tweets){
-			tweet=tweets[tweetn]
-			gj.push(
-			{
+	tweets = []
+	$.get('/allTweetLangs', function(response) {
+		tweets = response.tweets
+		gj = []
+		for (tweetn in tweets) {
+			tweet = tweets[tweetn]
+			gj.push({
 				type: 'Feature',
 				properties: {
-					language:tweet[0]
-				}, geometry: {
+					language: tweet[0]
+				},
+				geometry: {
 					type: 'Point',
 					coordinates: tweet[1]
 				}
@@ -140,9 +146,10 @@ var initializeMap = function() {
 		}
 		map.data.addGeoJson({
 			type: 'FeatureCollection',
-			features: gj})
+			features: gj
 		})
-	
+	})
+
 	// Leaving this here for nostalgia & in case the new way is too slow
 	/* $.get( "/languageslocations/-180/-90/180/90", function( response ) {
 		map.data.addGeoJson( response );
@@ -157,67 +164,92 @@ var initializeMap = function() {
 
 	//map.controls[google.maps.ControlPosition.TOP_LEFT].push( input );
 
-	var circle = $( "circle-canvas" ).circle();
+	var circle = $("circle-canvas").circle();
 
 	var lastBounds = false;
 	var updateLocation = function() {
-		
-	    var bounds = map.getBounds();
-		var countByLang={}
-		tweetCount=0
+		var bounds = map.getBounds();
+		var countByLang = {}
+		var tweetCount = 0
 		// # of segs not including "other"
 		var circlePortions = [];
 		var otherShare = 100;
 		var minShareSize = 1.0;
 		var shareLargeEnough = true;
-		for (tweetn in tweets){
-			var tweet=tweets[tweetn]
-			var lang=tweet[0]
-			var loc=tweet[1]
-			if(bounds.contains(new google.maps.LatLng(loc[1],loc[0]))){
-				if(lang in countByLang) countByLang[lang]+=1;
-				else countByLang[lang]=1;
-				tweetCount+=1
+
+		for (tweetn in tweets) {
+			var tweet = tweets[tweetn]
+			var lang = tweet[0]
+			var loc = tweet[1]
+			if (bounds.contains(new google.maps.LatLng(loc[1], loc[0]))) {
+				if (!(lang in countByLang))
+					countByLang[lang] = 0;
+				countByLang[lang] += 1;
+				tweetCount += 1
 			}
-			
 		}
-		langs=[]
-		for (lang in countByLang) {langs.push(lang)}
-		langs.sort(function(a,b){return countByLang[b]-countByLang[a]})
-		
-		
-		
+
+		langs = []
+		for (lang in countByLang) {
+			langs.push(lang)
+		}
+		langs.sort(function(a, b) {
+			return countByLang[b] - countByLang[a]
+		})
+
+
+
 		var languageShareHtml = "";
 		for (langn in langs) {
-			lang=langs[langn]
+			lang = langs[langn]
 			var languageTweetCount = countByLang[lang];
-			var languageTweetShare = ( languageTweetCount * 100 / tweetCount ).toFixed( 1 );
+			var languageTweetShare = (languageTweetCount * 100 / tweetCount).toFixed(1);
 
-			if ( languageTweetShare >= minShareSize ) {
-				var languageShareDisplay = "<div><img src=" + flags[lang] + " alt=" +
-							lang + "></img>: " + languageTweetShare + "%</div>\n";
-
-				if ( !( lang in flags ) ) {
-					languageShareDisplay = "<div>" + lang + ": " + languageTweetShare +
-						"%</div>\n";
+			if (languageTweetShare >= minShareSize) {
+				var languageShareDisplay = "<div><img src=" + flags[lang] + " alt=" + lang + "></img>: " + languageTweetShare + "%</div>\n";
+				if (!(lang in flags)) {
+					languageShareDisplay = "<div>" + lang + ": " + languageTweetShare + "%</div>\n";
 				}
 
 				languageShareHtml += languageShareDisplay;
 
-				circlePortions.push( parseFloat( languageTweetShare ) );
-				otherShare -= parseFloat( languageTweetShare );
+				circlePortions.push(parseFloat(languageTweetShare));
+				otherShare -= parseFloat(languageTweetShare);
 			}
 		}
-		$( "#languages" ).html( languageShareHtml );
-		if ( otherShare > 1 && otherShare < 100 ) {
-			circlePortions.push( otherShare );
-			circle.drawLangaugeSegments( circlePortions, true );
+		$("#languages").html(languageShareHtml);
+		if (otherShare > 1 && otherShare < 100) {
+			circlePortions.push(otherShare);
+			circle.drawLangaugeSegments(circlePortions, true);
 		} else {
-			circle.drawLangaugeSegments( circlePortions, false );
+			circle.drawLangaugeSegments(circlePortions, false);
 		}
-		
+
+
+		var bounds_sw = bounds.getSouthWest();
+		var bounds_ne = bounds.getNorthEast();
+		$.get( "/words/" + bounds_sw.lng() + "/" + bounds_sw.lat() + "/" + bounds_ne.lng() + "/" + bounds_ne.lat() + "/10", function( response ) {
+			if (response.words.length > 0) {
+				var words = response.words;
+				console.log(words);
+				for (var i = 0; i < words.length; i++) {
+					var word = words[i].word;
+					var count = words[i].count;
+					words[i] = {
+						text: word,
+						weight: count
+					}
+				}
+				$("#wordcloud").html("");
+				$("#wordcloud").jQCloud(words, {
+				  width: 180,
+				  height: 200,
+				  shape: "rectangular"
+				});
+			}
+		} );
 		/*
-		
+
 		// AJAX(map.getBounds().toString(),loadData)
 
 		// ignore changes smaller than 5%
@@ -335,17 +367,18 @@ var initializeMap = function() {
 			lastBounds = bounds;
 		}
 
-	};*/}
+	};*/
+	}
 
 	//updateLocation()
-	google.maps.event.addListener( map, "idle", updateLocation );
+	google.maps.event.addListener(map, "idle", updateLocation);
 
 	//temporarily ignore the sheer number of requests that would get sent here
 
 	//Search box, code from https://developers.google.com/maps/documentation/javascript/examples/places-autocomplete
 
-	var autocomplete = new google.maps.places.Autocomplete( input );
-	autocomplete.bindTo( "bounds", map );
+	var autocomplete = new google.maps.places.Autocomplete(input);
+	autocomplete.bindTo("bounds", map);
 
 	var infowindow = new google.maps.InfoWindow();
 	var marker = new google.maps.Marker({
@@ -355,22 +388,22 @@ var initializeMap = function() {
 		infowindow.open(map, marker);
 	});
 
-	google.maps.event.addListener( autocomplete, "place_changed", function() {
+	google.maps.event.addListener(autocomplete, "place_changed", function() {
 		infowindow.close();
 		var place = autocomplete.getPlace();
-		if ( !place.geometry ) {
+		if (!place.geometry) {
 			return;
 		}
 
 		// If the place has a geometry, then present it on a map.
-		if ( place.geometry.viewport ) {
-			map.fitBounds( place.geometry.viewport );
+		if (place.geometry.viewport) {
+			map.fitBounds(place.geometry.viewport);
 		} else {
-			map.setCenter( place.geometry.location );
+			map.setCenter(place.geometry.location);
 		}
 		map.setZoom(15);
 	});
 
 };
 
-google.maps.event.addDomListener( window, "load", initializeMap );
+google.maps.event.addDomListener(window, "load", initializeMap);
